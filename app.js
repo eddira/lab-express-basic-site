92d32e48e84fa5f21950284fc08e6c13785cff96
Mehdi Eddira <m.eddira75@outlook.fr>
2023-02-08 14:42:02 +0100

    add characters with their img from API + handling error

--- app.js
+++ app.js
@@ -1,36 +1,56 @@
-const express = require('express')
+const express = require("express");
 
-const hbs = require('hbs')
+const hbs = require("hbs");
 
-const app = express()
+const app = express();
 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
-app.set("view engine", "hbs")
-hbs.registerPartials(`${__dirname}/views/partials`)
+app.set("view engine", "hbs");
+hbs.registerPartials(`${__dirname}/views/partials`);
 
 app.get("/", (request, response) => {
   response.render("home", {
     navbar: true,
-  })
-})
-
-app.get("/about", (req, res) => {
-  console.log(req.query)
-  res.render("about", {
   });
 });
 
-app.get("/works", (req, res) => {
+app.get("/about", (req, res) => {
+  console.log(req.query);
+  res.render("about", {});
+});
+
+app.get("/works", async (req, res, next) => {
   console.log(req.query);
   res.render("works", {
     navbar: true,
-  })
-})
+  });
+});
+
+app.get("/characters", async (req, res, next) => {
+  try {
+    // throw Error('Oh no !')
+    const raw = await fetch("https://rickandmortyapi.com/api/character");
+    const finalResponse = await raw.json();
+    console.log(finalResponse);
+
+    res.render("characters", {
+      characters: finalResponse.results,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((error, req, res, next) => {
+  res.statusCode = 500;
+  console.log(error.message);
+  res.render("server-error", { code: res.statusCode, message: error.message });
+});
 
 app.listen(5000, (err) => {
   if (err) {
     return console.error(err);
   }
-  console.log(`http://localhost:5000`)
-})
+  console.log(`http://localhost:5000`);
+});
